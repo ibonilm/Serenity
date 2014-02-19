7d5e6b7175edd40f57855f10497949fffabc90fd
asier <asiermarques@gmail.com>
2014-02-19 12:52:44 +0100

    azure deployment file

---
+++ azure_composer.php
@@ -0,0 +1,13 @@
+<?php
+
+copy(__DIR__ . "/app/config/parameters.yml.azure", __DIR__ . "/app/config/parameters.yml");
+
+if (!file_exists("composer.phar")) {
+    $url = 'https://getcomposer.org/composer.phar';
+    file_put_contents("composer.phar", file_get_contents($url));
+}
+
+$_SERVER['argv'][1] = "update";
+$_SERVER['argv'][2] = "--prefer-dist";
+$_SERVER['argv'][3] = "-v";
+require "composer.phar";
